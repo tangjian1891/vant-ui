3494410522215e6254cb41489624119e453b7636
tangjian1891 <tangjian1891@163.com>
2021-08-16 17:41:05 +0800

    初始化empty样式，基础工具函数

--- src/demo-button2/index.vue
+++ src/demo-button2/index.vue
@@ -6,7 +6,7 @@
 
 <script>
 export default {
-  name: 'demo-button2',
+  name: 'DemoButton2',
 
   props: {
     color: String,
--- src/utils/with-install.ts
+++ src/utils/with-install.ts
@@ -10,17 +10,28 @@ type EventShim = {
   };
 };
 
-export type WithInstall<T> = T & {
-  install(app: App): void;
-} & EventShim;
+// export type WithInstall<T> = T & {
+//   install(app: App): void;
+// } & EventShim;
 
 // using any here because tsc will generate some weird results when using generics
+// export function withInstall<T>(options: any): WithInstall<T> {
+//   (options as Record<string, unknown>).install = (app: App) => {
+//     const { name } = options as any;
+//     app.component(name, options);
+//     app.component(camelize(`-${name}`), options);
+//   };
+
+//   return options;
+// }
+
+export type WithInstall<T> = T & { install(app: App): void } & EventShim;
+
 export function withInstall<T>(options: any): WithInstall<T> {
-  (options as Record<string, unknown>).install = (app: App) => {
-    const { name } = options as any;
+  (options as any).install = (app: App) => {
+    const { name } = options;
     app.component(name, options);
     app.component(camelize(`-${name}`), options);
   };
-
   return options;
 }
